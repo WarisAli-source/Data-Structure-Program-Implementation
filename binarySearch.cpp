b60c031037c0532995e2837c23ae4e258b530285
WarisAli-source <warissid41@gmail.com>
2020-12-29 18:46:02 +0530

    Binary Search Implementation Using C++

---
+++ binarySearch.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+int binarysearch(int arr[],int left,int right,int x)
+{
+	while(left<=right)
+	{
+		int mid=left + (right-left)/2;
+		if(arr[mid]==x)
+		{
+			return mid;
+		}
+		else if(arr[mid<x])
+		{
+			left=mid+1;
+		}
+		else
+		{
+			right=mid-1;
+		}
+	}
+	return -1;	
+	
+}
+int main(){
+	int num;
+	int myarr[10];
+	int output;
+	cout<<"Enter 10 Integers in ASCENDING Order"<<endl;
+	for(int i=0;i<10;i++)
+	{
+		cin>>myarr[i];
+		
+	}
+	cout<<"Enter one number you want to search in the Array:\n"<<endl;
+	cin>>num;
+	output=binarysearch(myarr,0,9,num);
+	if(output==-1)
+	{
+		cout<<"No match Found"<<endl;
+	}
+	else
+	{
+		cout<<"Match found at index position :"<<output<<endl;
+	}
+	return 0;
+}
